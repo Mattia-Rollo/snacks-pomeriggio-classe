64fb4d7b086530e4675770fae6f7569c832ffd21
Mattia-Rollo <ste.tia@hotmail.it>
2022-10-15 19:50:43 +0200

    add comment

--- js/script.js
+++ js/script.js
@@ -48,6 +48,8 @@
 // Genera N array, ognuno formato da 10 numeri casuali da 1 a 100.
 // Ogni volta che ne crei uno, stampalo.
 
+
+// creo una finzione simile a array.push() 
 function myPushArray(array,elem) {
     array[array.length] = elem;
 }
